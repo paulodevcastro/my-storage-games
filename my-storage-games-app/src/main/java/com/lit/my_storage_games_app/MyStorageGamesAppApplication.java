d19539c6a228065fe40b62b9cc7b14522d7ab0a9
Gui <guizits200@gmail.com>
2025-01-29 19:08:07 -0300

    Starter project

---
+++ my-storage-games-app/src/main/java/com/lit/my_storage_games_app/MyStorageGamesAppApplication.java
@@ -0,0 +1,13 @@
+package com.lit.my_storage_games_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStorageGamesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStorageGamesAppApplication.class, args);
+	}
+
+}
---
+++ my-storage-games-app/src/test/java/com/lit/my_storage_games_app/MyStorageGamesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lit.my_storage_games_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStorageGamesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
